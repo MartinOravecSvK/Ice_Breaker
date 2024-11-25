d59d29d136ee128d7502216521fbcc48b6347e8e
MartinOravecSvK <oravec.martin01@gmail.com>
2024-11-25 06:20:29 +0000

    update

---
+++ workspace/examples/vuln_program_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void vulnerable_function() {
+    char buffer[64];
+    printf("Enter some text: ");
+    gets(buffer);
+}
+
+int main() {
+    vulnerable_function();
+    printf("Returned safely to main.\n");
+    return 0;
+}
